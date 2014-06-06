2f2dc3c74b5197754a05ca8e71d06626c95a6e5c
agentem <eeporta@gmail.com>
2014-06-06 15:06:16 -0400

    first commit

---
+++ page-home.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+	Template Name: Home Page
+*/ 
+
+get_header();  ?>
+
+<div class="section">
+  <div class="innerWrapper">
+    <div class="full">
+      
+    </div>
+  </div> <!-- /.innerWrapper -->
+</div> <!-- /.section -->
+<?php get_footer(); ?>

c59e8526819739b210215386a8ced55c66501301
agentem <eeporta@gmail.com>
2014-06-07 17:31:10 -0400

    updates
    git push origin maste
    git push origin master
    git commit -m updates!

--- page-home.php
+++ page-home.php
@@ -6,11 +6,13 @@
 
 get_header();  ?>
 
-<div class="section">
   <div class="innerWrapper">
-    <div class="full">
-      
-    </div>
+    <div class="about full" id="about">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/h2OrnamentLeft.jpg">
+      <h2>About Me</h2>
+      <img src="<?php echo get_template_directory_uri(); ?>/images/h2OrnamentRight.jpg">
+      <p>Hi, I'm Emily. I'm a front-end web developer who loves creating great looking web products with rock-solid code. I'm currently looking for a position as a junior developer at a company who values life-long learning and passion for innovation.</p>
+      <div class="button">Contact Me<div>
+    </div> <!-- /.about -->
   </div> <!-- /.innerWrapper -->
-</div> <!-- /.section -->
 <?php get_footer(); ?>
